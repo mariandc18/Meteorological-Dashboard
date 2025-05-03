4d6749f8b1ac3852c9216698dc872bab514fbaec
mariandc18 <ma907222@gmail.com>
2025-05-03 18:41:54 -0400

    change functions for multiple locs data ingestion

--- src/data_ingestion/historical.py
+++ src/data_ingestion/historical.py
@@ -4,6 +4,8 @@ import pandas as pd
 from retry_requests import retry
 from pymongo import MongoClient
 from datetime import datetime
+from transformation import convert_time_OpenMeteoAPI, split_datetime
+
 
 # Setup the Open-Meteo API client with cache and retry on error
 cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
@@ -14,143 +16,158 @@ openmeteo = openmeteo_requests.Client(session = retry_session)
 client = MongoClient('mongodb://localhost:27017/')
 db = client['weather_db']
 weather_collection = db['weather_data']
+#weather_collection.delete_many({})
+locations_collection = db['locations']
 
 # Make sure all required weather variables are listed here
 url = "https://archive-api.open-meteo.com/v1/archive"
-params = {
-    "latitude": 23.133,
-    "longitude": -82.383,
-    "start_date": "2000-04-13",
-    "end_date": "2025-04-27",
-    "daily": ["temperature_2m_mean", "temperature_2m_max", "temperature_2m_min", "sunrise", "sunset", "precipitation_sum", "snowfall_sum", "wind_speed_10m_max"],
-	"hourly": ["temperature_2m", "relative_humidity_2m", "dew_point_2m", "apparent_temperature", "precipitation", "cloud_cover", "wind_speed_10m", "wind_gusts_10m", "wind_direction_10m", "snowfall", "pressure_msl", "is_day"]
-}
 
-print("Fetching data from Open-Meteo API...")
-responses = openmeteo.weather_api(url, params=params)
+def process_location(location):
+    params = {
+        "latitude": location["location"]["coordinates"][1],
+        "longitude": location["location"]["coordinates"][0],
+        "start_date": "2025-04-13",
+        "end_date": "2025-04-27",
+        "daily": ["temperature_2m_mean", "temperature_2m_max", "temperature_2m_min", "sunrise", "sunset", "precipitation_sum", "snowfall_sum", "wind_speed_10m_max"],
+        "hourly": ["temperature_2m", "relative_humidity_2m", "dew_point_2m", "apparent_temperature", "precipitation", "cloud_cover", "wind_speed_10m", "wind_gusts_10m", "wind_direction_10m", "snowfall", "pressure_msl", "is_day"]
+    }
 
-# Process first location. Add a for-loop for multiple locations or weather models
-response = responses[0]
-print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
-print(f"Elevation {response.Elevation()} m asl")
-print(f"Timezone {response.Timezone()}{response.TimezoneAbbreviation()}")
-print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+    print(f"\nProcessing data for {location['municipio']}, {location['provincia']}...")
+    responses = openmeteo.weather_api(url, params=params)
+    response = responses[0]
 
-print("\nProcessing hourly data...")
-# Process hourly data
-hourly = response.Hourly()
-hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
-hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
-hourly_dew_point_2m = hourly.Variables(2).ValuesAsNumpy()
-hourly_apparent_temperature = hourly.Variables(3).ValuesAsNumpy()
-hourly_precipitation = hourly.Variables(4).ValuesAsNumpy()
-hourly_cloud_cover = hourly.Variables(5).ValuesAsNumpy()
-hourly_wind_speed_10m = hourly.Variables(6).ValuesAsNumpy()
-hourly_wind_gusts_10m = hourly.Variables(7).ValuesAsNumpy()
-hourly_wind_direction_10m = hourly.Variables(8).ValuesAsNumpy()
-hourly_snowfall = hourly.Variables(9).ValuesAsNumpy()
-hourly_pressure_msl = hourly.Variables(10).ValuesAsNumpy()
-hourly_is_day = hourly.Variables(11).ValuesAsNumpy()
+    # Process hourly data
+    hourly = response.Hourly()
+    hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+    hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
+    hourly_dew_point_2m = hourly.Variables(2).ValuesAsNumpy()
+    hourly_apparent_temperature = hourly.Variables(3).ValuesAsNumpy()
+    hourly_precipitation = hourly.Variables(4).ValuesAsNumpy()
+    hourly_cloud_cover = hourly.Variables(5).ValuesAsNumpy()
+    hourly_wind_speed_10m = hourly.Variables(6).ValuesAsNumpy()
+    hourly_wind_gusts_10m = hourly.Variables(7).ValuesAsNumpy()
+    hourly_wind_direction_10m = hourly.Variables(8).ValuesAsNumpy()
+    hourly_snowfall = hourly.Variables(9).ValuesAsNumpy()
+    hourly_pressure_msl = hourly.Variables(10).ValuesAsNumpy()
+    hourly_is_day = hourly.Variables(11).ValuesAsNumpy()
 
-# Create date range for hourly data
-hourly_dates = pd.date_range(
-    start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
-    end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
-    freq = pd.Timedelta(seconds = hourly.Interval()),
-    inclusive = "left"
-)
+    # Create date range for hourly data
+    hourly_dates = pd.date_range(
+        start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+        end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+        freq = pd.Timedelta(seconds = hourly.Interval()),
+        inclusive = "left"
+    )
 
-# Create hourly data dictionary with separated date and time as strings
-hourly_data = {
-    "date": [d.strftime('%Y-%m-%d') for d in hourly_dates],
-    "time": [d.strftime('%H:%M:%S') for d in hourly_dates],
-    "data_type": ["hourly"] * len(hourly_dates),
-    "temperature": hourly_temperature_2m,
-    "relative_humidity": hourly_relative_humidity_2m,
-    "dew_point": hourly_dew_point_2m,
-    "apparent_temperature": hourly_apparent_temperature,
-    "precipitation": hourly_precipitation,
-    "pressure": hourly_pressure_msl,
-    "cloud_cover": hourly_cloud_cover,
-    "wind_speed": hourly_wind_speed_10m,
-    "wind_direction": hourly_wind_direction_10m,
-    "wind_gusts": hourly_wind_gusts_10m
-}
+    date_str, time_str = split_datetime(hourly_dates)
 
-hourly_dataframe = pd.DataFrame(data = hourly_data)
-print("Hourly data sample:")
-print(hourly_dataframe.head())
+    # Create hourly data dictionary with location information
+    hourly_data = {
+        "date": date_str,
+        "time": time_str,
+        "data_type": ["hourly"] * len(hourly_dates),
+        "country": [location["country"]] * len(hourly_dates),
+        "provincia": [location["provincia"]] * len(hourly_dates),
+        "municipio": [location["municipio"]] * len(hourly_dates),
+        #"location": [location["location"]] * len(hourly_dates),
+        "temperature": hourly_temperature_2m,
+        "relative_humidity": hourly_relative_humidity_2m,
+        "dew_point": hourly_dew_point_2m,
+        "apparent_temperature": hourly_apparent_temperature,
+        "precipitation": hourly_precipitation,
+        "pressure": hourly_pressure_msl,
+        "cloud_cover": hourly_cloud_cover,
+        "wind_speed": hourly_wind_speed_10m,
+        "wind_direction": hourly_wind_direction_10m,
+        "wind_gusts": hourly_wind_gusts_10m
+    }
 
-print("\nProcessing daily data...")
-# Process daily data
-daily = response.Daily()
-daily_temperature_2m_mean = daily.Variables(0).ValuesAsNumpy()
-daily_temperature_2m_max = daily.Variables(1).ValuesAsNumpy()
-daily_temperature_2m_min = daily.Variables(2).ValuesAsNumpy()
-daily_sunrise = daily.Variables(3).ValuesInt64AsNumpy()
-daily_sunset = daily.Variables(4).ValuesInt64AsNumpy()
-daily_precipitation_sum = daily.Variables(5).ValuesAsNumpy()
-daily_snowfall_sum = daily.Variables(6).ValuesAsNumpy()
-daily_wind_speed_10m_max = daily.Variables(7).ValuesAsNumpy()
+    hourly_dataframe = pd.DataFrame(data = hourly_data)
 
-# Create date range for daily data
-daily_dates = pd.date_range(
-    start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
-    end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
-    freq = pd.Timedelta(seconds = daily.Interval()),
-    inclusive = "left"
-)
+    # Process daily data
+    daily = response.Daily()
+    daily_temperature_2m_mean = daily.Variables(0).ValuesAsNumpy()
+    daily_temperature_2m_max = daily.Variables(1).ValuesAsNumpy()
+    daily_temperature_2m_min = daily.Variables(2).ValuesAsNumpy()
+    daily_sunrise = daily.Variables(3).ValuesInt64AsNumpy()
+    daily_sunset = daily.Variables(4).ValuesInt64AsNumpy()
+    daily_precipitation_sum = daily.Variables(5).ValuesAsNumpy()
+    daily_snowfall_sum = daily.Variables(6).ValuesAsNumpy()
+    daily_wind_speed_10m_max = daily.Variables(7).ValuesAsNumpy()
 
-# Create daily data dictionary with date as string (no time field needed for daily data)
-daily_data = {
-    "date": [d.strftime('%Y-%m-%d') for d in daily_dates],
-    "data_type": ["daily"] * len(daily_dates),
-    "temperature_mean": daily_temperature_2m_mean,
-    "temperature_max": daily_temperature_2m_max,
-    "temperature_min": daily_temperature_2m_min,
-    "sunrise": daily_sunrise,
-    "sunset": daily_sunset,
-    "precipitation_sum": daily_precipitation_sum,
-    "snowfall_sum": daily_snowfall_sum,
-    "wind_speed_10m_max": daily_wind_speed_10m_max   
-}
+    # Create date range for daily data
+    daily_dates = pd.date_range(
+        start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+        end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+        freq = pd.Timedelta(seconds = daily.Interval()),
+        inclusive = "left"
+    )
 
-daily_dataframe = pd.DataFrame(data = daily_data)
-print("Daily data sample:")
-print(daily_dataframe.head())
+    date_str, _ = split_datetime(daily_dates)
 
-combined_dataframe = pd.concat([hourly_dataframe, daily_dataframe], ignore_index=True)
-weather_records = combined_dataframe.to_dict('records')
+    # Create daily data dictionary with location information
+    daily_data = {
+        "date": date_str,
+        "data_type": ["daily"] * len(daily_dates),
+        "country": [location["country"]] * len(daily_dates),
+        "provincia": [location["provincia"]] * len(daily_dates),
+        "municipio": [location["municipio"]] * len(daily_dates),
+        #"location": [location["location"]] * len(daily_dates),
+        "temperature_mean": daily_temperature_2m_mean,
+        "temperature_max": daily_temperature_2m_max,
+        "temperature_min": daily_temperature_2m_min,
+        "sunrise": [convert_time_OpenMeteoAPI(ts) for ts in daily_sunrise],
+        "sunset": [convert_time_OpenMeteoAPI(ts) for ts in daily_sunset],
+        "precipitation_sum": daily_precipitation_sum,
+        "snowfall_sum": daily_snowfall_sum,
+        "wind_speed_10m_max": daily_wind_speed_10m_max   
+    }
 
-print("\nChecking for existing records...")
-existing_hourly = set((doc['date'], doc.get('time', '')) for doc in weather_collection.find(
-    {'data_type': 'hourly'},
-    {'date': 1, 'time': 1, '_id': 0}
-))
-existing_daily = set(doc['date'] for doc in weather_collection.find(
-    {'data_type': 'daily'},
-    {'date': 1, '_id': 0}
-))
+    daily_dataframe = pd.DataFrame(data = daily_data)
+    return pd.concat([hourly_dataframe, daily_dataframe], ignore_index=True)
 
-new_records = []
-for record in weather_records:
-    if record['data_type'] == 'hourly':
-        if (record['date'], record['time']) not in existing_hourly:
-            new_records.append(record)
+# Get all locations from MongoDB
+locations = list(locations_collection.find({}))
+print(f"Found {len(locations)} locations to process")
+
+# Process each location
+for location in locations:
+    print(f"\nProcessing data for {location['municipio']}, {location['provincia']}...")
+    combined_dataframe = process_location(location)
+    weather_records = combined_dataframe.to_dict('records')
+
+    # Check for existing records for this location
+    existing_hourly = set((doc['date'], doc.get('time', ''), doc['municipio']) 
+                        for doc in weather_collection.find(
+                            {'data_type': 'hourly', 'municipio': location['municipio']},
+                            {'date': 1, 'time': 1, 'municipio': 1, '_id': 0}
+                        ))
+    
+    existing_daily = set((doc['date'], doc['municipio']) 
+                       for doc in weather_collection.find(
+                           {'data_type': 'daily', 'municipio': location['municipio']},
+                           {'date': 1, 'municipio': 1, '_id': 0}
+                       ))
+
+    new_records = []
+    for record in weather_records:
+        if record['data_type'] == 'hourly':
+            if (record['date'], record['time'], record['municipio']) not in existing_hourly:
+                new_records.append(record)
+        else:
+            if (record['date'], record['municipio']) not in existing_daily:
+                new_records.append(record)
+
+    if new_records:
+        print(f"Inserting {len(new_records)} new records for {location['municipio']}...")
+        weather_collection.insert_many(new_records)
+        print(f"Data successfully inserted for {location['municipio']}!")
     else:
-        if record['date'] not in existing_daily:
-            new_records.append(record)
-
-if new_records:
-    print(f"Inserting {len(new_records)} new records...")
-    weather_collection.insert_many(new_records)
-    print("Data successfully inserted into MongoDB!")
-else:
-    print("No new records to insert. All data already exists in the database.")
+        print(f"No new records to insert for {location['municipio']}. All data already exists in the database.")
 
 print(f"\nDatabase statistics:")
 print(f"Total records in database: {weather_collection.count_documents({})}")
 print(f"Hourly records: {weather_collection.count_documents({'data_type': 'hourly'})}")
 print(f"Daily records: {weather_collection.count_documents({'data_type': 'daily'})}")
 
-client.close()  
+client.close() 
--- src/data_ingestion/up_to_date.py
+++ src/data_ingestion/up_to_date.py
@@ -2,158 +2,151 @@ import requests
 from datetime import datetime, timezone, timedelta
 from pymongo import MongoClient
 from transformation import convert_time_WheatherAPI, split_datetime
+import time
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['weather_db']
 weather_collection = db['weather_data']
-#weather_collection.delete_many({})
+locations_collection = db['locations']
 
 # WeatherAPI configuration
 API_KEY = "API Key"
-LOCATION = "40.4168,-3.7038"
 HISTORY_URL = "https://api.weatherapi.com/v1/history.json"
 
-def fetch_hourly_data():
-    try:
-        current_time = datetime.now(timezone.utc)
-        current_hour = current_time.hour
-        today = current_time.strftime("%Y-%m-%d")
-        
-        print(f"\nFetching data for date: {today}")
-        print(f"Current time (UTC): {current_time.strftime('%H:%M')}")
-        
-        params = {"key": API_KEY, "q": LOCATION, "dt": today}
-        resp = requests.get(HISTORY_URL, params=params)
-        resp.raise_for_status()
-        data = resp.json()
+def fetch_hourly_data(location):
+    current_time = datetime.now(timezone.utc)
+    current_hour = current_time.hour
+    today = current_time.strftime("%Y-%m-%d")
+    
+    print(f"\nFetching data for {location['municipio']}, {location['provincia']} - {today}")
+    print(f"Current time (UTC): {current_time.strftime('%H:%M')}")
+    
+    coords = f"{location['location']['coordinates'][1]},{location['location']['coordinates'][0]}"
+    params = {"key": API_KEY, "q": coords, "dt": today}
+    
+    resp = requests.get(HISTORY_URL, params=params, timeout=60)
+    resp.raise_for_status()
+    data = resp.json()
 
-        # Filtrar horas hasta la hora actual
-        hours = data["forecast"]["forecastday"][0]["hour"]
-        target_hours = [h for h in hours if datetime.fromtimestamp(h["time_epoch"], tz=timezone.utc).hour <= current_hour]
-        
-        if target_hours:
-            first_hour = datetime.fromtimestamp(target_hours[0]["time_epoch"], tz=timezone.utc)
-            last_hour = datetime.fromtimestamp(target_hours[-1]["time_epoch"], tz=timezone.utc)
-            print(f"Data range: from {first_hour.strftime('%H:%M')} to {last_hour.strftime('%H:%M')} UTC")
+    # Filtrar horas hasta la hora actual
+    hours = data["forecast"]["forecastday"][0]["hour"]
+    target_hours = [h for h in hours if datetime.fromtimestamp(h["time_epoch"], tz=timezone.utc).hour <= current_hour]
+    
+    if target_hours:
+        first_hour = datetime.fromtimestamp(target_hours[0]["time_epoch"], tz=timezone.utc)
+        last_hour = datetime.fromtimestamp(target_hours[-1]["time_epoch"], tz=timezone.utc)
+        print(f"Data range: from {first_hour.strftime('%H:%M')} to {last_hour.strftime('%H:%M')} UTC")
 
-        # Preparar los datos para Mongo
-        records = []
-        for h in target_hours:
-            dt = datetime.fromtimestamp(h["time_epoch"], tz=timezone.utc)
-            date, time = split_datetime(dt)
-            rec = {
-                "date": date,
-                "time": time,
-                "data_type": "hourly",
-                "temperature": h.get("temp_c"),         
-                "relative_humidity": h.get("humidity"),   
-                "dew_point": h.get("dewpoint_c"),          
-                "apparent_temperature": h.get("feelslike_c"), 
-                "precipitation": h.get("precip_mm"),      
-                "pressure": h.get("pressure_mb"),          
-                "cloud_cover": h.get("cloud"),             
-                "wind_speed": h.get("wind_kph"),           
-                "wind_direction": h.get("wind_degree"),    
-                "wind_gusts": h.get("gust_kph"),          
-                "is_day": h.get("is_day")                  
-            }
-            records.append(rec)
+    # Preparar los datos para Mongo
+    records = []
+    for h in target_hours:
+        dt = datetime.fromtimestamp(h["time_epoch"], tz=timezone.utc)
+        date, time = split_datetime(dt)
+        rec = {
+            "date": date,
+            "time": time,
+            "data_type": "hourly",
+            "country": location["country"],
+            "provincia": location["provincia"],
+            "municipio": location["municipio"],
+            #"location": location["location"],
+            "temperature": h.get("temp_c"),         
+            "relative_humidity": h.get("humidity"),   
+            "dew_point": h.get("dewpoint_c"),          
+            "apparent_temperature": h.get("feelslike_c"), 
+            "precipitation": h.get("precip_mm"),      
+            "pressure": h.get("pressure_mb"),          
+            "cloud_cover": h.get("cloud"),             
+            "wind_speed": h.get("wind_kph"),           
+            "wind_direction": h.get("wind_degree"),    
+            "wind_gusts": h.get("gust_kph"),          
+            "is_day": h.get("is_day")                  
+        }
+        records.append(rec)
 
-        existing_records = set(
-            (d.get("date", ""), d.get("time", "")) 
-            for d in weather_collection.find(
-                {"data_type": "hourly", "date": today}, 
-                {"date": 1, "time": 1, "_id": 0}
-            )
+    existing_records = set(
+        (d.get("date", ""), d.get("time", ""), d.get("municipio", "")) 
+        for d in weather_collection.find(
+            {"data_type": "hourly", "date": today, "municipio": location["municipio"]}, 
+            {"date": 1, "time": 1, "municipio": 1, "_id": 0}
         )
-        
-        new_records = [r for r in records if (r["date"], r["time"]) not in existing_records]
+    )
+    
+    new_records = [r for r in records if (r["date"], r["time"], r["municipio"]) not in existing_records]
 
-        if new_records:
-            weather_collection.insert_many(new_records)
-            print(f"Inserted {len(new_records)} new records.")
-            print(f"First record: {new_records[0]['date']} {new_records[0]['time']}")
-            print(f"Last record: {new_records[-1]['date']} {new_records[-1]['time']}")
-        else:
-            print("No new records to insert.")
+    if new_records:
+        weather_collection.insert_many(new_records)
+        print(f"Inserted {len(new_records)} new records for {location['municipio']}.")
+        print(f"First record: {new_records[0]['date']} {new_records[0]['time']}")
+        print(f"Last record: {new_records[-1]['date']} {new_records[-1]['time']}")
+    else:
+        print(f"No new records to insert for {location['municipio']}.")
 
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching weather data: {e}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-def fetch_and_update_daily():
+def fetch_and_update_daily(location):
     yesterday = datetime.now(timezone.utc).date() - timedelta(days=1)
     date, _ = split_datetime(datetime.combine(yesterday, datetime.min.time()))
 
     # Comprobar si ya existe daily
     existing_doc = weather_collection.find_one({
         "date": date,
-        "data_type": "daily"
+        "data_type": "daily",
+        "municipio": location["municipio"]
     })
 
+    coords = f"{location['location']['coordinates'][1]},{location['location']['coordinates'][0]}"
+    params = {"key": API_KEY, "q": coords, "dt": yesterday.strftime("%Y-%m-%d")}
+    
+    resp = requests.get(HISTORY_URL, params=params, timeout=60)
+    resp.raise_for_status()
+    data = resp.json()
+    day = data["forecast"]["forecastday"][0]["day"]
+
+    # Construir documento daily
+    daily_doc = {
+        "date": date,
+        "data_type": "daily",
+        "country": location["country"],
+        "provincia": location["provincia"],
+        "municipio": location["municipio"],
+        #"location": location["location"],
+        "temperature_mean": day.get("avgtemp_c"),
+        "temperature_max": day.get("maxtemp_c"),
+        "temperature_min": day.get("mintemp_c"),
+        "sunrise": convert_time_WheatherAPI(data["forecast"]["forecastday"][0]["astro"].get("sunrise")),
+        "sunset": convert_time_WheatherAPI(data["forecast"]["forecastday"][0]["astro"].get("sunset")),
+        "precipitation_sum": day.get("totalprecip_mm"),
+        "snowfall_sum": day.get("totalsnow_cm"),
+        "wind_speed_10m_max": day.get("maxwind_kph")
+    }
+
     if existing_doc:
-        print(f"Ya existe un registro daily para {date}. Verificando si hay cambios...")
-        # Obtener datos nuevos
-        params = {"key": API_KEY, "q": LOCATION, "dt": yesterday.strftime("%Y-%m-%d")}
-        resp = requests.get(HISTORY_URL, params=params)
-        resp.raise_for_status()
-        data = resp.json()
-        day = data["forecast"]["forecastday"][0]["day"]
-
-        # Construir documento daily
-        daily_doc = {
-            "date": date,
-            "data_type": "daily",
-            "temperature_mean": day.get("avgtemp_c"),
-            "temperature_max": day.get("maxtemp_c"),
-            "temperature_min": day.get("mintemp_c"),
-            "sunrise": convert_time_WheatherAPI(data["forecast"]["forecastday"][0]["astro"].get("sunrise")),
-            "sunset": convert_time_WheatherAPI(data["forecast"]["forecastday"][0]["astro"].get("sunset")),
-            "precipitation_sum": day.get("totalprecip_mm"),
-            "snowfall_sum": day.get("totalsnow_cm"),
-            "wind_speed_10m_max": day.get("maxwind_kph")
-        }
-
+        print(f"Verificando cambios en registro daily para {location['municipio']} - {date}...")
         changes = {}
         for key, value in daily_doc.items():
-            if key not in ("date", "data_type") and existing_doc.get(key) != value:
+            if key not in ("date", "data_type", "country", "provincia", "municipio", "location") and existing_doc.get(key) != value:
                 changes[key] = value
         
         if changes:
-            # Solo se actualiza si hay cambios
             weather_collection.update_one(
                 {"_id": existing_doc["_id"]},
                 {"$set": changes}
             )
-            print(f"Documento daily para {date} actualizado con campos: {', '.join(changes.keys())}.")
+            print(f"Documento daily para {location['municipio'], location['provincia'], location['country'], date} actualizado con campos: {', '.join(changes.keys())}.")
         else:
-            print(f"Documento daily para {date} ya está actualizado, no hay cambios.")
+            print(f"Documento daily para {location['municipio'], location['provincia'], location['country'], date} ya está actualizado, no hay cambios.")
     else:
-        # Si no existe, obtener e insertar nuevos datos
-        params = {"key": API_KEY, "q": LOCATION, "dt": yesterday.strftime("%Y-%m-%d")}
-        resp = requests.get(HISTORY_URL, params=params)
-        resp.raise_for_status()
-        data = resp.json()
-        day = data["forecast"]["forecastday"][0]["day"]
-
-        daily_doc = {
-            "date": date,
-            "data_type": "daily",
-            "temperature_mean": day.get("avgtemp_c"),
-            "temperature_max": day.get("maxtemp_c"),
-            "temperature_min": day.get("mintemp_c"),
-            "sunrise": convert_time_WheatherAPI(data["forecast"]["forecastday"][0]["astro"].get("sunrise")),
-            "sunset": convert_time_WheatherAPI(data["forecast"]["forecastday"][0]["astro"].get("sunset")),
-            "precipitation_sum": day.get("totalprecip_mm"),
-            "snowfall_sum": day.get("totalsnow_cm"),
-            "wind_speed_10m_max": day.get("maxwind_kph")
-        }
-
         weather_collection.insert_one(daily_doc)
-        print(f"Documento daily para {date} insertado.")
+        print(f"Documento daily para {location['municipio'], location['provincia'], location['country'], date} insertado.")
 
 if __name__ == "__main__":
-    fetch_hourly_data()
-    fetch_and_update_daily()
+    locations = list(locations_collection.find({}))
+    print(f"Found {len(locations)} locations to process")
+    
+    for location in locations:
+        print(f"\nProcessing {location['municipio']}, {location['provincia']}...")
+        fetch_hourly_data(location)
+        time.sleep(5)
+        fetch_and_update_daily(location)
+        time.sleep(5)
+    
     client.close()
