4a575494c783f6b006e59f66dcc4bb4a5e79dc21
mariandc18 <ma907222@gmail.com>
2025-05-06 19:30:30 -0400

    make some changes in rest api

--- src/serving/model_rest_api.py
+++ src/serving/model_rest_api.py
@@ -1,19 +1,17 @@
-# api.py
 from fastapi import FastAPI
-from pydantic import BaseModel
-from datetime import datetime, timedelta
-from db_manager import MongoDBManager
 import pandas as pd
 import numpy as np
 import joblib
-import uvicorn
+from pydantic import BaseModel
+from typing import Optional
 
-weather_collection = MongoDBManager(db_name="weather_db", collection_name="weather_data")
+
+def load_model(path='model_new.pkl'):
+    model_data = joblib.load(path)
+    return model_data['model'], model_data['features']
+
+model, features = load_model()
 app = FastAPI()
-
-model_data = joblib.load('model.pkl')
-model = model_data['model']
-features = model_data['features']
 
 class InputData(BaseModel):
     date: str
@@ -28,11 +26,13 @@ class InputData(BaseModel):
     wind_gusts: float
     wind_direction: float
     pressure: float
+    ubicacion_id: Optional[str] = None
 
-def preprocess_input(data):
-    data['datetime'] = pd.to_datetime(data['date'] + ' ' + data['time'])
+def preprocess_input(data: pd.DataFrame) -> pd.DataFrame:
+    
+    if 'datetime' not in data.columns or data['datetime'].dtype == object:
+        data['datetime'] = pd.to_datetime(data['date'] + ' ' + data['time'], errors='coerce')
     data = data.sort_values('datetime').reset_index(drop=True)
-
     data['hour'] = data['datetime'].dt.hour
     data['month'] = data['datetime'].dt.month
     data['dayofweek'] = data['datetime'].dt.dayofweek
@@ -40,32 +40,22 @@ def preprocess_input(data):
     for col, max_val in [('hour', 24), ('month', 12), ('dayofweek', 7)]:
         data[f'{col}_sin'] = np.sin(2 * np.pi * data[col] / max_val)
         data[f'{col}_cos'] = np.cos(2 * np.pi * data[col] / max_val)
+
     data['temp_humidity'] = data['temperature'] * data['relative_humidity']
-    
     return data[features]
 
 @app.post('/predict')
-def predict(input_data: InputData):
-    # Obtener la fecha y hora actuales
-    end_time = datetime.now()
-    start_time = end_time - timedelta(hours=48)
-    # Consultar los datos de las últimas 48 horas
-    recent_data = list(weather_collection.find({
-        "datetime": {
-            "$gte": start_time,
-            "$lte": end_time
-        },
-        "data_type": "hourly"  
-    }))
-
-    if not recent_data:
-        return {"error": "No hay suficientes datos para realizar la predicción."}
+def predict(user_data: Optional[list[InputData]] = None):
+    if not user_data:
+        return {"error": "No se han enviado datos para la predicción."}
     
-    df = pd.DataFrame(recent_data)
-    df['datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'])
+    df = pd.DataFrame([data.dict() for data in user_data])
+    df['datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'], errors='coerce')
+    
+    if df['datetime'].isnull().all():
+        return {"error": "No se pudieron convertir los campos 'date' y 'time' correctamente."}
+
     processed_data = preprocess_input(df)
     predictions = model.predict(processed_data)
-    return {"predictions": predictions.tolist()}
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    return {"predictions": predictions.tolist()}
