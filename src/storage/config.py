1c69edf1462ce03a848e73eceee7f5b8ec178c7e
mariandc18 <ma907222@gmail.com>
2025-05-23 16:00:16 -0400

    add setup file for PostgreSQL database

---
+++ src/storage/config.py
@@ -0,0 +1,9 @@
+import os
+
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require&connect_timeout=10"
