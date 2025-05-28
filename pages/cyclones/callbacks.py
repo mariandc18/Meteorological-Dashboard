6a982a45a696e3b94e500dddbe405867e993fd70
mariandc18 <ma907222@gmail.com>
2025-05-28 01:02:29 -0400

    fix some errors in callbacks.py

--- pages/cyclones/callbacks.py
+++ pages/cyclones/callbacks.py
@@ -11,9 +11,15 @@ def register_callbacks(app):
         Output('cyclone-dropdown', 'options'),
         Input('season-slider', 'value')
     )
-    def update_cyclone_dropdown(season):
+    def update_cyclone_dropdown(season_range):
+        start_year, end_year = season_range
         session: Session = get_db_session()
-        names = session.query(CycloneTrajectory.name).filter_by(season=season).distinct().all()
+        names = (
+            session.query(CycloneTrajectory.name)
+            .filter(CycloneTrajectory.season >= start_year, CycloneTrajectory.season <= end_year)
+            .distinct()
+            .all()
+        )
         return [{'label': name[0], 'value': name[0]} for name in names]
 
     @app.callback(
@@ -21,7 +27,7 @@ def register_callbacks(app):
         Input('cyclone-dropdown', 'value'),
         Input('season-slider', 'value')
     )
-    def plot_single_cyclone(name, season):
+    def plot_single_cyclone(name, season_range):
         session = get_db_session()
         if not name:
             fig = go.Figure(go.Scattermapbox())
@@ -29,10 +35,16 @@ def register_callbacks(app):
             fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
             return fig
 
+        start_year, end_year = season_range
         df = pd.read_sql(
             session.query(CycloneTrajectory)
-                .filter_by(name=name, season=season)
-                .statement, session.bind
+                .filter(
+                    CycloneTrajectory.name == name,
+                    CycloneTrajectory.season.between(start_year, end_year)
+                )
+                .order_by(CycloneTrajectory.iso_time)
+                .statement,
+            session.bind
         )
         fig = px.line_mapbox(df, lat='lat', lon='lon', hover_data=['iso_time', 'usa_status'],
                             color_discrete_sequence=['blue'], zoom=4, height=400)
@@ -46,33 +58,37 @@ def register_callbacks(app):
     Input('cyclone-dropdown', 'value'),
     Input('season-slider', 'value')
 )
-    def cyclone_details_plot(name, season):
+    def cyclone_details_plot(name, season_range):
         session = get_db_session()
-        if not name:
+
+        if not name or not season_range:
             empty_fig = px.line()
             return empty_fig, empty_fig, empty_fig
 
-        df = pd.read_sql(
+        start_year, end_year = season_range
+        query = (
             session.query(CycloneTrajectory)
-                .filter_by(name=name, season=season)
-                .order_by(CycloneTrajectory.iso_time)
-                .statement, session.bind
+            .filter(
+                CycloneTrajectory.name == name,
+                CycloneTrajectory.season.between(start_year, end_year)
+            )
+            .order_by(CycloneTrajectory.iso_time)
         )
 
+        df = pd.read_sql(query.statement, session.bind)
         fig_sshs = px.line(df, x='iso_time', y='usa_sshs',
                         title='Categoría SSHS',
                         labels={'iso_time': 'Fecha', 'usa_sshs': 'Categoría Saffir-Simpson'})
 
         fig_dist2land = px.line(df, x='iso_time', y='dist2land',
-                        title='Distancia a Tierra',
-                        labels={'iso_time': 'Fecha', 'dist2land': 'Distancia (km)'})
+                                title='Distancia a Tierra',
+                                labels={'iso_time': 'Fecha', 'dist2land': 'Distancia (km)'})
 
         fig_landfall = px.line(df, x='iso_time', y='landfall',
-                            title='Landfall',
+                            title='Landfall (1 = sí)',
                             labels={'iso_time': 'Fecha', 'landfall': 'Landfall'})
 
         return fig_sshs, fig_dist2land, fig_landfall
-
 
     @app.callback(
         Output('all-cyclones-paths', 'figure'),
@@ -80,12 +96,14 @@ def register_callbacks(app):
         Output('pressure-comparison', 'figure'),
         Input('season-slider', 'value')
     )
-    def compare_cyclones(season):
+    def compare_cyclones(season_range):
+        start_year, end_year = season_range
         session = get_db_session()
         df = pd.read_sql(
             session.query(CycloneTrajectory)
-                   .filter_by(season=season)
-                   .statement, session.bind
+            .filter(CycloneTrajectory.season >= start_year, CycloneTrajectory.season <= end_year)
+            .statement,
+            session.bind
         )
         df['step'] = df.groupby('name').cumcount()
         fig_paths = px.line_mapbox(df, lat='lat', lon='lon', color='name', hover_data=['step'],
