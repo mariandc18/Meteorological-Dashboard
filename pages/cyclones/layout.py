9b772692761f5f2a9936aab4a9cc45e9b0a602c5
mariandc18 <ma907222@gmail.com>
2025-05-27 22:54:49 -0400

    update layout for cyclones

--- pages/cyclones/layout.py
+++ pages/cyclones/layout.py
@@ -15,19 +15,25 @@ cyclone_layout = html.Div([
     ),
 
     html.Br(),
+    
+    html.H3("Comparación de todos los ciclones en la temporada"),
+    dcc.Graph(id='all-cyclones-paths'),
+    dcc.Graph(id='wind-comparison'),
+    dcc.Graph(id='pressure-comparison'),
+
+    html.Hr(),
+    
     html.Label("Selecciona un ciclón:"),
     dcc.Dropdown(id='cyclone-dropdown', placeholder='Selecciona un ciclón', multi=False),
 
     html.Hr(),
+
     html.H3("Trayectoria del ciclón seleccionado"),
     dcc.Graph(id='cyclone-path-map'),
 
-    html.H3("Evolución del viento y presión del ciclón"),
-    dcc.Graph(id='cyclone-wind-pressure'),
-
-    html.Hr(),
-    html.H3("Comparación de todos los ciclones en la temporada"),
-    dcc.Graph(id='all-cyclones-paths'),
-    dcc.Graph(id='wind-comparison'),
-    dcc.Graph(id='pressure-comparison')
+    html.H3("Evolución del ciclón"),
+    dcc.Graph(id='graph-sshs'),
+    dcc.Graph(id='graph-dist2land'),
+    dcc.Graph(id='graph-landfall'),
+    
 ])
