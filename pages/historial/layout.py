c167061707783dd884241d490f2315ace35c8479
mariandc18 <ma907222@gmail.com>
2025-07-10 11:38:59 -0400

    add button in historial to redirect to view

--- pages/historial/layout.py
+++ pages/historial/layout.py
@@ -2,25 +2,25 @@ from dash import html, dcc
 from dash.dash_table import DataTable
 
 layout = html.Div([
-    html.H2("Historial de Interacciones", className="header"),
-    
+    html.H2("Historial de Vistas Guardadas", className="header"),
+
     dcc.Store(id="historial-interactions-store"),
 
     html.Div([
         DataTable(
             id="tabla-historial",
             columns=[
+                {"name": "Nombre", "id": "name"},
                 {"name": "Página", "id": "page"},
-                {"name": "Componente", "id": "component_id"},
-                {"name": "Valor", "id": "value"},
                 {"name": "Fecha", "id": "timestamp"},
+                {"name": "Ir", "id": "go", "presentation": "markdown"},
                 {"name": "Eliminar", "id": "delete", "presentation": "markdown"}
             ],
             data=[],
             style_table={"overflowX": "auto"},
             style_cell={"textAlign": "left"},
         )
-    ], className="card"),  
-    
+    ], className="card"),
+
     html.Div(id="feedback-eliminar", className="feedback-msg")
 ], className="main-container")
