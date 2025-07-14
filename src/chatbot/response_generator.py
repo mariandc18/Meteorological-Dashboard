fc1370fd315f2f7ec14f60cada0c0d20b93b88b5
mariandc18 <ma907222@gmail.com>
2025-07-14 03:44:21 -0400

    modify response generator to use gemini-2.5-flash

--- src/chatbot/response_generator.py
+++ src/chatbot/response_generator.py
@@ -5,6 +5,7 @@ from datetime import timedelta, date
 import requests
 import numpy as np
 import os
+import requests
 
 def load_prompt(file_path: str) -> str:
     with open(file_path, "r", encoding="utf-8") as f:
@@ -17,35 +18,42 @@ def custom_json_encoder(obj):
         return float(obj)
     raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
 
-def generate_llm_input(location, aggregated):
+def generate_llm_input(location: str, aggregated: list[AggregatedForecast]) -> dict:
     return ForecastAggregator([]).prepare_llm_input(location, aggregated)
 
-def assemble_prompt(prompt_text, llm_input):
+def assemble_prompt(prompt_text: str, llm_input: dict) -> str:
     context_str = json.dumps(llm_input, indent=2, ensure_ascii=False, default=custom_json_encoder)
     return f"{prompt_text}\n\nContexto:\n{context_str}"
 
-def call_llm(prompt, model= "mistral"):
-    url = "http://localhost:11434/api/generate"
-    payload = {
-        "model": model,
-        "prompt": prompt,
-        "stream": False  
+def call_llm(prompt, model="gemini-2.5-flash"):
+    api_key = os.getenv("GEMINI_API_KEY")
+    url = f"https://generativelanguage.googleapis.com/v1/models/{model}:generateContent"
+    headers = {
+        "Content-Type": "application/json",
+        "x-goog-api-key": api_key
     }
+    payload = {
+        "contents": [
+            {"role": "user", "parts": [{"text": prompt}]}
+        ]
+    }
+
     try:
-        response = requests.post(url, json=payload)
+        response = requests.post(url, headers=headers, json=payload)
         response.raise_for_status()
         data = response.json()
-        return data["response"].strip()
+        return data["candidates"][0]["content"]["parts"][0]["text"].strip()
     except requests.exceptions.RequestException as e:
         print("Error:", e)
-        return "Hubo un error al conectarse con el modelo"
+        return "Hubo un error al conectarse con Gemini"
+
 
 def generate_forecast_response(location, aggregator, user_prompt, precomputed=None):
     if precomputed:
         aggregated = precomputed
     else:
-        range = (date.today(), date.today() + timedelta(days=7))
-        aggregated = aggregator.aggregate_daily_forecasts(location, range)
+        rango = (date.today(), date.today() + timedelta(days=7))
+        aggregated = aggregator.aggregate_daily_forecasts(location, rango)
 
     llm_input = generate_llm_input(location.name, aggregated)
     llm_input["user_prompt"] = user_prompt  
